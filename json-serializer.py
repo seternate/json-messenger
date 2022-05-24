2404ba614c1d707b0fccb0807f7f10d6b5fa56f5
Fabianovic <unehw@student.kit.edu>
2022-05-24 18:21:49 +0200

    Check if the Json format is legit

--- json-serializer.py
+++ json-serializer.py
@@ -1,6 +1,13 @@
 import json
 
+def jsonCheck (string):
+    bool = False
+    try:
+        jsonObject = json.loads(string)
+        bool = True
+    except:
+        bool = False
+    return bool
 
-a = input('Penis')
 
-print(a)
+
